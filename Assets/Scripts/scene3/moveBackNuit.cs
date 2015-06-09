73bfa70cf18fa87c61283a65ed4d36c5ce643915
CauzYolo <william.decool@gmail.com>
2015-06-09 14:48:54 +0200

    Modification des scripts gerant la mise en place des backGrounds et sols ainsi que la position de ceux ci

--- Assets/Scripts/scene3/moveBackNuit.cs
+++ Assets/Scripts/scene3/moveBackNuit.cs
@@ -1,1 +1,62 @@
+using UnityEngine;
+using System.Collections;
+
+public class moveBackNuit : MonoBehaviour {
+	
+	public float positionRestartX;
+	Vector2 movement = new Vector2 (-2f, 0f);
+	Vector2 siz;
+	Vector3 leftBottomCameraBorder;
+	float pos = -99;
+	
+	
+	void Start(){
+		siz.x =	gameObject.GetComponent<SpriteRenderer>	().bounds.size.x * transform.localScale.x;	
+		siz.y =	gameObject.GetComponent<SpriteRenderer>	().bounds.size.y * transform.localScale.y;	
+		
+		leftBottomCameraBorder = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, 0));
+		
+	}
+	
+	void Update()	
+	{	
+		rigidbody2D.velocity = movement;	
+		
+		//	Si	le	sprite	sort	de	l'écran	à	gauche	
+		//	Recalcul	d'une	nouvelle	posi=on	en	Y	comprise	dans	les	limites	autorisées	de	l'écran
+		//Reposi=onnement	en	X		à	la	posi=on	d’origine	de	bacGround3	
+		
+		if	(transform.position.x <	leftBottomCameraBorder.x - (siz.x /	2))	
+		{	
+			if(gameObject.tag == "back"){
+				GameObject[] backGrounds = GameObject.FindGameObjectsWithTag("back") as GameObject[];
+				foreach (GameObject backGround in backGrounds) {
+					float currPos = backGround.transform.position.x;
+					Debug.Log("currPos : " + currPos);
+					if(currPos > pos){
+						Debug.Log("currPos > pos : pos=" + pos + ", currPos=" + currPos);
+						pos = currPos;
+						positionRestartX = currPos+siz.x/2-.6f;
+					}
+				}
+				transform.position	= new Vector3(positionRestartX,transform.position.y,transform.position.z);	
+				Debug.Log("PositionRestart : " + positionRestartX);
+			}
+			else if(gameObject.tag == "sol"){
+				GameObject[] sols = GameObject.FindGameObjectsWithTag("sol") as GameObject[];
+				foreach (GameObject sol in sols) {
+					float currPos = sol.transform.position.x;
+					Debug.Log("currPos : " + currPos);
+					if(currPos > pos){
+						Debug.Log("currPos > pos : pos=" + pos + ", currPos=" + currPos);
+						pos = currPos;
+						positionRestartX = currPos+siz.x/2-.6f;
+					}
+				}
+				transform.position	= new Vector3(positionRestartX,transform.position.y,transform.position.z);	
+				Debug.Log("PositionRestart : " + positionRestartX);
+			}
+		}	
+	}	
+}
 
--- Assets/Scripts/scene3/posBackNuit.cs
+++ Assets/Scripts/scene3/posBackNuit.cs
@@ -1,1 +1,42 @@
+using UnityEngine;
+using System.Collections;
+
+
+public class posBackNuit : MonoBehaviour {
+	
+	Vector2 siz;
+	public SpriteRenderer backGround;
+	public SpriteRenderer sol;
+	
+	
+	// Use this for initialization
+	void Start () {
+		siz.x =	gameObject.GetComponent<SpriteRenderer>	().bounds.size.x * transform.localScale.x;	
+		siz.y =	gameObject.GetComponent<SpriteRenderer>	().bounds.size.y * transform.localScale.y;	
+
+		if (tag == "back") {
+			SpriteRenderer back2 = Instantiate (backGround, new Vector3 (transform.position.x + siz.x / 2 - .6f, transform.position.y, transform.position.z), Quaternion.identity) as SpriteRenderer;
+			back2.name = "backGround2";
+			back2.tag = "back";
+			SpriteRenderer back3 = Instantiate (backGround, new Vector3 (transform.position.x + 2 * (siz.x / 2 - .6f), transform.position.y, transform.position.z), Quaternion.identity) as SpriteRenderer;
+			back3.name = "backGround3";
+			back3.tag = "back";
+		} 
+		else if (tag == "sol") {
+			SpriteRenderer sol2 = Instantiate (sol, new Vector3 (transform.position.x + siz.x / 2 - .6f, transform.position.y, transform.position.z), Quaternion.identity) as SpriteRenderer;
+			sol2.name = "sol2";
+			sol2.tag = "sol";
+			SpriteRenderer sol3 = Instantiate (sol, new Vector3 (transform.position.x + 2 * (siz.x / 2 - .6f), transform.position.y, transform.position.z), Quaternion.identity) as SpriteRenderer;
+			sol3.name = "sol3";
+			sol3.tag = "sol";
+
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
 
