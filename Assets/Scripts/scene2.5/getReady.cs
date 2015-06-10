e1a6c001ecb67e4efece80d618a594c16bed31d6
CauzYolo <william.decool@gmail.com>
2015-06-10 15:17:10 +0200

    Ajout du script changeant de scene pour la game quand le premier font sort de l'écran

---
+++ Assets/Scripts/scene2.5/getReady.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class getReady : MonoBehaviour {
+
+	private Vector3 coinSupGauche;
+	GameObject back;
+	Vector3 backSiz;
+	GameObject spriteGetReady;
+
+	// Use this for initialization
+	void Start () {
+
+		coinSupGauche = Camera.main.ViewportToWorldPoint (new Vector3 (0, 1, 0));
+		back = GameObject.Find("backGroundNuit");
+		backSiz = back.GetComponent<SpriteRenderer> ().bounds.size;
+		spriteGetReady = GameObject.Find ("getReady");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (back.transform.position.x + (backSiz.x/2) <= coinSupGauche.x) {
+			Debug.Log("READY!");
+			Application.LoadLevel(2);
+
+		}
+	
+	}
+}
