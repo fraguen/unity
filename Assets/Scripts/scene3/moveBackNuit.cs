dc685505b79fbc57c3f93071d1a49cbe7e1d341f
CauzYolo <william.decool@gmail.com>
2015-06-11 11:25:18 +0200

    Ajout de random pour invoquer les nuages

--- Assets/Scripts/scene3/moveBackNuit.cs
+++ Assets/Scripts/scene3/moveBackNuit.cs
@@ -6,8 +6,12 @@ public class moveBackNuit : MonoBehaviour {
 	public float positionRestartX;
 	Vector2 movement = new Vector2 (-2f, 0f);
 	Vector2 siz;
-	Vector3 leftBottomCameraBorder;
+	Vector3 leftBottomCameraBorder, rightBottomCameraBorder;
 	float pos = -99;
+	public GameObject nuageSmallGrey, nuageNormalGrey;
+	private int randomNuageGenerator;
+	private int frameCount = 0;
+
 	
 	
 	void Start(){
@@ -15,16 +19,25 @@ public class moveBackNuit : MonoBehaviour {
 		siz.y =	gameObject.GetComponent<SpriteRenderer>	().bounds.size.y * transform.localScale.y;	
 		
 		leftBottomCameraBorder = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, 0));
+		rightBottomCameraBorder = Camera.main.ViewportToWorldPoint(new Vector3 (1, 0, 0));
+		randomNuageGenerator = (int)generateRandom (500f, 1000f);
+
 		
 	}
 	
 	void Update()	
 	{	
 		if(!ScoreManager.instance.isOnGameOver()){
+			frameCount++;
 			if(gameObject.tag == "back")
 				rigidbody2D.velocity = movement;	
 			else if(gameObject.tag == "sol")
 				rigidbody2D.velocity = new Vector2(-5f, 0f);
+			if(frameCount == randomNuageGenerator){
+				frameCount = 0;
+				generateNuage((int)generateRandom(1,100));
+				randomNuageGenerator = (int)generateRandom(500f, 1000f);
+			}
 		}
 		else{
 			rigidbody2D.velocity = new Vector2(0f, 0f);
@@ -65,6 +78,28 @@ public class moveBackNuit : MonoBehaviour {
 				//Debug.Log("PositionRestart : " + positionRestartX);
 			}
 		}	
-	}	
+	}
+
+	float generateRandom(float start, float end){
+		return Random.Range (start, end);
+	}
+
+	void generateNuage(int type){
+
+		Vector2 mouvement = new Vector2(-5f, 0f);
+
+		if(type%2 != 0){
+			float posNuageY =  generateRandom(0f, 7f);
+			float posNuageX = rightBottomCameraBorder.x + nuageSmallGrey.GetComponent<SpriteRenderer>().bounds.size.x;
+			GameObject nuage = Instantiate (nuageSmallGrey, new Vector3 (posNuageX, posNuageY, nuageSmallGrey.transform.position.z), Quaternion.identity) as GameObject;
+			nuage.rigidbody2D.velocity = mouvement;
+		}
+		else{
+			float posNuageY =  generateRandom(0f, 7f);
+			float posNuageX = rightBottomCameraBorder.x + nuageNormalGrey.GetComponent<SpriteRenderer>().bounds.size.x;
+			GameObject nuage = Instantiate (nuageNormalGrey, new Vector3 (posNuageX, posNuageY, nuageNormalGrey.transform.position.z), Quaternion.identity) as GameObject;
+			nuage.rigidbody2D.velocity = mouvement;
+		}
+	}
 }
 
