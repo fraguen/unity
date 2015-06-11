bb019ee8196ea9a5011a410c6a1ed4fcccfa6b02
CauzYolo <william.decool@gmail.com>
2015-06-11 11:26:12 +0200

    modif movePipes => Ajout des plantes, modif posBackNuit

--- Assets/Scripts/scene3/movePipes.cs
+++ Assets/Scripts/scene3/movePipes.cs
@@ -6,9 +6,11 @@ public class movePipes : MonoBehaviour {
 	public GameObject pipe1Up;
 	public GameObject pipe1Down;
 	public GameObject betweenPipes;
+	public GameObject plante;
 	private Transform pipe1UpOriginalTransform;
 	private Transform pipe1DownOriginalTransform;
 	private Transform betweenPipesTransform;
+	private Transform planteTransform;
 
 	private	Vector3	coinBasGauche;
 	private	Vector3	coinBasDroit;
@@ -29,6 +31,9 @@ public class movePipes : MonoBehaviour {
 		pipe1UpOriginalTransform = pipe1Up.transform;
 		pipe1DownOriginalTransform = pipe1Down.transform;
 		betweenPipesTransform = betweenPipes.transform;
+		float posYPlante = pipe1Down.transform.position.y + pipe1Down.GetComponent<SpriteRenderer> ().bounds.size.y / 2 + plante.GetComponent<SpriteRenderer> ().bounds.size.y / 2;
+		Vector3 tmpPos = new Vector3 (plante.transform.position.x, posYPlante, plante.transform.position.z);
+		plante.transform.position = tmpPos;
 	}
 	
 	// Update is called once per frame
@@ -37,18 +42,20 @@ public class movePipes : MonoBehaviour {
 			pipe1Up.rigidbody2D.velocity = mouvement;
 			pipe1Down.rigidbody2D.velocity = mouvement;
 			betweenPipes.rigidbody2D.velocity = mouvement;
+			plante.rigidbody2D.velocity = mouvement;
 		}
 		else{
 			mouvement = new Vector2(0f, 0f);
 			pipe1Up.rigidbody2D.velocity = mouvement;
 			pipe1Down.rigidbody2D.velocity = mouvement;
 			betweenPipes.rigidbody2D.velocity = mouvement;
+			plante.rigidbody2D.velocity = mouvement;
 		}
 		siz.x =	pipe1Up.GetComponent<SpriteRenderer>().bounds.size.x;
 		siz.y =	pipe1Up.GetComponent<SpriteRenderer>().bounds.size.y;
 		if (pipe1Up.transform.position.x < coinBasGauche.x - (siz.x / 2)) {
-						moveToRightPipe ();
-				}
+			moveToRightPipe ();
+		}
 		/*if (transform.position.x < coinHautGauche.x || transform.position.x < coinBasGauche.x) {
 			transform.position=new Vector3(horsCadre,transform.position.y,transform.position.z);
 		}*/
@@ -58,6 +65,7 @@ public class movePipes : MonoBehaviour {
 		float randomY =	Random.Range(1,4)-2;
 		float posX =coinBasDroit.x+(siz.x/2);
 		float posY = pipe1UpOriginalTransform.position.y+randomY;
+		float posYPlante;
 
 		Vector3	tmpPos = new Vector3(posX, posY, pipe1Up.transform.position.z);
 		pipe1Up.transform.position = tmpPos;
@@ -69,6 +77,10 @@ public class movePipes : MonoBehaviour {
 		posY = (pipe1Up.transform.position.y + pipe1Down.transform.position.y) / 2;
 		tmpPos = new Vector3(posX,posY,betweenPipes.transform.position.z);
 		betweenPipes.transform.position = tmpPos;
+
+		posYPlante = pipe1Down.transform.position.y + pipe1Down.GetComponent<SpriteRenderer> ().bounds.size.y / 2 + plante.GetComponent<SpriteRenderer> ().bounds.size.y / 2;
+		tmpPos = new Vector3 (posX, posYPlante, plante.transform.position.z);
+		plante.transform.position = tmpPos;
 
 	}
 }
